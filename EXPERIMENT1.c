8bb53280cde5511490a1ea685655deed5895efb6
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-03-07 21:16:50 +0530

    Add files via upload

---
+++ EXPERIMENT1.c
@@ -0,0 +1,14 @@
+main()
+{
+    void add(void);
+    system("cls");
+    add();
+    getch();
+}
+void add()
+{
+int a,b;
+printf("Enter two numbers");
+scanf("%d%d",&a,&b);
+printf("Addition is %d",a+b);
+}
