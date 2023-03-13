7b4f7fd50438a109923ca2b4a6fbd49df96ef938
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-03-13 13:00:10 +0530

    Add files via upload

---
+++ L6LADDER.c
@@ -0,0 +1,17 @@
+main()
+{
+   int a;
+   system("cls");
+   printf("Enter any number");
+   scanf("%d",&a);
+   if(a>90)
+   printf("Grade A");
+   else if(a>80)
+   printf("Grade B");
+   else if(a>70)
+   printf("Grade C");
+   else
+   printf("Grade D");
+   getch();
+
+}
---
+++ L6P2.c
@@ -0,0 +1,15 @@
+main ()
+{   int x;
+    system("cls");
+    printf("Enter any Number");
+    scanf("%d",&x);
+    if (x>0)
+    {
+        printf("Ajinkya is invincible");
+    }
+    else
+    {
+        printf("Ajinkya is indispensable");
+    }
+    getch();
+}
